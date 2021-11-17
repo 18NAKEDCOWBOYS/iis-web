82aad992cedd0cb2631ea9acdf43116a163bac98
Jkotoun <jkotoun58@gmail.com>
2021-11-17 17:06:11 +0100

    Add auction list admin functions

--- src/components/AuctionCard.js
+++ src/components/AuctionCard.js
@@ -2,19 +2,38 @@ import React from 'react';
 import Button from 'react-bootstrap/Button'
 import Card from 'react-bootstrap/Card'
 import Styles from '../css/previewCard.module.css'
+
+function ApprovalButtons(loggedUserRoleId)
+{
+  //admin or auctioneer
+  if(loggedUserRoleId >=2 )
+  {
+    return(
+      <div className={Styles.buttonsContainer}>
+          <Button variant="success" className={Styles.buttonApprove}>Schválit</Button>{' '}
+          <Button variant="danger" className={Styles.buttonReject}>Zamítnout</Button>
+      </div>
+    )
+  }
+}
+
 export default function AuctionCard(props) {
+  const loggedUser = {
+    "roleId" : 2
+  }
   return (
    
     <Card style={{width:400}}>
       <Card.Img variant="top" src={props.preview_image} />
       <Card.Body>
-        <Card.Title>{props.title}</Card.Title>
+        <Card.Title className={Styles.cardTitle}>{props.title}</Card.Title>
         <Card.Text>
          <div className={Styles.cardItem}> <strong>Typ:</strong> {props.type}<br/></div>
          <div className={Styles.cardItem}> <strong> Pravidla:</strong> {props.rules}<br/></div>
          <div className={Styles.cardItem}> <strong>Vyvolávací cena:</strong> {props.price} Kč<br/></div>
+        {(loggedUser.roleId>=2 && props.auctioneer_id != undefined)  && (props.approved? <div className={Styles.approvedText}>Schválena</div> : <div className={Styles.rejectedText}>Zamítnuta</div>)}
         </Card.Text>
-        <Button variant="primary">Zobrazit detail</Button>
+        {props.auctioneer_id == undefined && ApprovalButtons(loggedUser.roleId)} 
       </Card.Body>
     </Card>
     )
--- src/pages/AuctionsPage.js
+++ src/pages/AuctionsPage.js
@@ -3,54 +3,71 @@ import React from 'react'
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
-
+import Form from 'react-bootstrap/Form';
 import NavigationBar from '../components/NavigationBar';
 import RegForm from '../components/RegForm';
 
 import Styles from './../css/AuctionsPage.module.css'
 import AuctionCard from '../components/AuctionCard';
-
-
+import { Link } from 'react-router-dom';
 
 
 export default function AuctionsPage(props) {
-  const auctions = [
+  const loggedUser = {
+    "roleId": 2
+    
+  }
+  let auctions = [
     {
       "name": "Aukce A",
       "type": "Nabídka",
       "rules": "Otevřená",
       "price": 12354,
-      "images": ["https://www.signfix.com.au/wp-content/uploads/2017/09/placeholder-600x400.png"]
+      "images": ["https://www.signfix.com.au/wp-content/uploads/2017/09/placeholder-600x400.png"],
+      "approved": true,
+      "auctioneer_id": 2
     },
     {
       "name": "Aukce B",
       "type": "Nabídka",
       "rules": "Uzavřená",
       "price": 50,
-      "images": ["https://www.signfix.com.au/wp-content/uploads/2017/09/placeholder-600x400.png"]
+      "images": ["https://www.signfix.com.au/wp-content/uploads/2017/09/placeholder-600x400.png"],
+      "approved": false,
+      "auctioneer_id": undefined
     },
     {
       "name": "Aukce C",
       "type": "Poptávka",
       "rules": "Otevřená",
       "price": 542133,
-      "images": ["https://www.signfix.com.au/wp-content/uploads/2017/09/placeholder-600x400.png"]
+      "images": ["https://www.signfix.com.au/wp-content/uploads/2017/09/placeholder-600x400.png"],
+      "approved": false,
+      "auctioneer_id": 2
     },
     {
       "name": "Aukce D",
       "type": "Nabídka",
       "rules": "Otevřená",
       "price": 2,
-      "images": ["https://www.signfix.com.au/wp-content/uploads/2017/09/placeholder-600x400.png"]
+      "images": ["https://www.signfix.com.au/wp-content/uploads/2017/09/placeholder-600x400.png"],
+      "approved": true,
+      "auctioneer_id": 1
     },
     {
       "name": "Aukce E",
       "type": "Poptávka",
       "rules": "Uzavřená",
       "price": 7823,
-      "images": ["https://www.signfix.com.au/wp-content/uploads/2017/09/placeholder-600x400.png"]
+      "images": ["https://www.signfix.com.au/wp-content/uploads/2017/09/placeholder-600x400.png"],
+      "approved": false,
+      "auctioneer_id": undefined
     }
   ]
+  //not admin or auctioneer
+  if (loggedUser.roleId < 2) {
+    auctions = auctions.filter(item => item.approved == true)
+  }
   return (
     <>
       <NavigationBar page="auctions" />
@@ -60,7 +77,10 @@ export default function AuctionsPage(props) {
         <Container className={Styles.flexContainer}>
           {auctions.map(item => {
             return (
-              <div style={{ padding: 15 }}> <AuctionCard title={item.name} type={item.type} rules={item.rules} price={item.price} preview_image={item.images[0]} /></div>
+              <div style={{ padding: 15 }}>
+    
+                <AuctionCard title={item.name} type={item.type} rules={item.rules} price={item.price} preview_image={item.images[0]} approved = {item.approved} auctioneer_id = {item.auctioneer_id}/>
+              </div>
             )
           })}
 
