57a7e9599d4209673716b739a7a9656ec849ad77
xbenes55 <xbenes55@stud.fit.vutbr.cz>
2021-11-18 03:48:07 +0100

    Add modal for new auction !!DOES NOT WORK!!

--- src/pages/AuctionsPage.js
+++ src/pages/AuctionsPage.js
@@ -1,12 +1,81 @@
 import React from 'react'
 
 import Container from 'react-bootstrap/Container';
+import Modal from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
 import NavigationBar from '../components/NavigationBar';
 import Styles from './../css/AuctionsPage.module.css'
 import AuctionCard from '../components/AuctionCard';
+import AuctionForm from '../components/AuctionForm';
+import {Formik} from 'formik'
 
+function NewAuctionModal(props) {
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title style={{color:"#0d6efd"}} id="contained-modal-title-vcenter">
+          Tvorba nové aukce
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Formik
+              initialValues={{ email: '', password: '', name:'', surname:'', role:null}}
+              validate={values => {
+                const errors = {};
+                if(!values.email){
+                  errors.email = 'Nezadali jste e-mailovou adresu';
+                }else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+                  errors.email = 'Nesprávný formát e-mailové adresy';
+                }
+                
+                if(!values.password){
+                  errors.password = 'Nezadali jste heslo';
+                }else if (values.password.length < 8){
+                  errors.password = 'Heslo je příliš krátké';
+                }
+
+                if(!values.name){
+                  errors.name='Nezadali jste jméno'
+                }
+                
+                if(!values.surname){
+                  errors.surname='Nezadali jste příjmení'
+                }
+                if(!values.role){
+                  errors.role = 'Vyberte prosím roli uživatele'
+                }
+                return errors;
+              }}
+              onSubmit={(values, bag) => {
+                setTimeout(() => {
+                  console.log('This will run after 2 second!');
+                  bag.setSubmitting(false);
+                }, 2000);
+              }}
+            >
+              {({
+               values,
+               errors,
+               status,
+               touched,
+               handleChange,
+               handleSubmit,
+               isSubmitting,
+              }) => <AuctionForm onHide={props.onHide} values={values} errors={errors} status={status} touched={touched} handleChange={handleChange} handleSubmit={handleSubmit} isSubmitting={isSubmitting}/>}
+            </Formik>
+      </Modal.Body>
+    </Modal>
+  );
+}
 
 export default function AuctionsPage(props) {
+  const [newAuctionModalShow, setNewAuctionModalShow] = React.useState(false);
+
   const loggedUser = {
     "roleId": 2
     
@@ -73,6 +142,9 @@ export default function AuctionsPage(props) {
       <Container className="mainContainer">
 
         <h1>Přehled aukcí</h1>
+        <div style={{width:'100%', padding:'10px'}}>
+          <Button variant='primary' onClick={() => setNewAuctionModalShow(true)} >Přidat novou aukci</Button>
+        </div>
         <Container className={Styles.flexContainer}>
           {auctions.map(item => {
             return (
@@ -82,6 +154,7 @@ export default function AuctionsPage(props) {
             )
           })}
         </Container>
+
       </Container>
     </>
   )
