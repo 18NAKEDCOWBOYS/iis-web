25fa1204d8837b927982a823e9238d79728df0bb
Jkotoun <jkotoun58@gmail.com>
2021-11-20 16:40:43 +0100

    Fix user profile rendering

--- src/pages/UserProfilePage.js
+++ src/pages/UserProfilePage.js
@@ -8,39 +8,57 @@ export default function UserProfilePage() {
 
     let myAuctions = [
         {
-            "id": 0,
+            "id":0,
             "name": "Aukce A",
-            "type": "Nabídka",
-            "rules": "Otevřená",
+            "is_demand": false,
+            "is_open": true,
             "price": 12354,
-            "images": ["https://www.signfix.com.au/wp-content/uploads/2017/09/placeholder-600x400.png"],
-            "approved": true,
+            "author_id": 1,
             "auctioneer_id": 2,
-            "author_id": 0
-        }]
+            "start_time":  new Date(2021, 12, 20, 10, 0, 0, 0),
+            "end_time": new Date(2021, 12, 2, 10, 0, 0, 0),
+            "description": "Skvělá aukce o věc v hodnotě milionů!",
+            "photos": ["https://www.signfix.com.au/wp-content/uploads/2017/09/placeholder-600x400.png", "https://pbs.twimg.com/profile_images/949787136030539782/LnRrYf6e_400x400.jpg", "https://pbs.twimg.com/media/E12BaOYXMAQeV9a.jpg", "https://c8.alamy.com/comp/EPF1YW/nun-with-handgun-isolated-on-white-EPF1YW.jpg", "https://ichef.bbci.co.uk/news/976/cpsprodpb/13F00/production/_95146618_bills.jpg"],
+            "winner_id": null,
+            "state_id":1, //schvaleno
+            "min_bid": 250,
+            "max_bid": null
+          }]
 
     let followedAuctions = [
         {
             "id":3,
             "name": "Aukce D",
-            "type": "Nabídka",
-            "rules": "Otevřená",
-            "price": 2,
-            "images": ["https://www.signfix.com.au/wp-content/uploads/2017/09/placeholder-600x400.png"],
-            "approved": true,
-            "auctioneer_id": 1,
-            "author_id": 2
+            "is_demand": false,
+            "is_open": true,
+            "price": 5613,
+            "author_id": 0,
+            "auctioneer_id": 2,
+            "start_time":  new Date(2021, 11, 18, 10, 0, 0, 0),
+            "end_time": new Date(2021, 11, 22, 10, 0, 0, 0),
+            "description": "Skvělá aukce o věc v hodnotě milionů!",
+            "photos": ["https://www.signfix.com.au/wp-content/uploads/2017/09/placeholder-600x400.png"],
+            "winner_id": null,
+            "state_id":3, //probihajici
+            "min_bid": 250,
+            "max_bid": null
           },
           {
             "id":4,
-            "name": "Aukce E",
-            "type": "Poptávka",
-            "rules": "Uzavřená",
-            "price": 7823,
-            "images": ["https://www.signfix.com.au/wp-content/uploads/2017/09/placeholder-600x400.png"],
-            "approved": false,
-            "auctioneer_id": undefined,
-            "author_id": 1
+            "name": "Aukce D",
+            "is_demand": false,
+            "is_open": true,
+            "price": 5613,
+            "author_id": 0,
+            "auctioneer_id": 1,
+            "start_time":  new Date(2021, 10, 17, 10, 0, 0, 0),
+            "end_time": new Date(2021, 10, 18, 10, 0, 0, 0),
+            "description": "Skvělá aukce o věc v hodnotě milionů!",
+            "photos": ["https://www.signfix.com.au/wp-content/uploads/2017/09/placeholder-600x400.png"],
+            "winner_id": null,
+            "state_id":1, //schvalena
+            "min_bid": 250,
+            "max_bid": null
           }
     ]
 
