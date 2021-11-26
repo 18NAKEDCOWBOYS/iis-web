89b8c4fa5a5cfa8a7e83bc3421d350b85a61770c
xbenes55 <xbenes55@stud.fit.vutbr.cz>
2021-11-26 18:37:31 +0100

    Generalized error page

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import Button from 'react-bootstrap/Button'
 import Container from 'react-bootstrap/Container'
-import Styles from './../css/Unauthorized.module.css'
+import Styles from './../css/ErrorPage.module.css'
 
-export default function Unauthorized(props) {
+export default function ErrorPage(props) {
     return (
         <>
             <Container className={Styles.centerContent}>
-                <h1 style={{ fontSize: 90 }}>Error 401</h1>
-                <h2> Unauthorized</h2>
+                <h1 style={{ fontSize: 90 }}>{props.errorId}</h1>
+                <h2> {props.errorText}</h2>
                 <Button variant='primary' href='/' size="lg" style={{ margin: 20 }}>Home</Button>
             </Container>
         </>
